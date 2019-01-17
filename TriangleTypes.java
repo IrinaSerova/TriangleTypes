f993cd559dd0a4eb53cf221cd0a07fbc8f3301dc
Irina Serova <isserova@gmail.com>
2019-01-17 12:08:03 -0800

    images added

--- TriangleTypes.java
+++ TriangleTypes.java
@@ -2,23 +2,13 @@ import java.util.Scanner;
 
 //triangles may be
 //
-//Scalene (all sides are different)
-//Isosceles (two sides are equal)
-//Equilateral (all three sides are equal)
+//Scalene (all angles are different)
+//Isosceles (two angles are equal)
+//Equilateral (all three angles are equal)
 
 
-// program reads in three angles (in degrees).
-// 
-//• If all three angles are 60, output Equilateral.
-//• If the three angles add up to 180 and exactly two of the angles are the same, output Isosceles.
-//• If the three angles add up to 180 and no two angles are the same, output Scalene.
-//• If the three angles do not add up to 180, output Error.
-// 
-//Input Specification The input consists of three integers, each on a separate line.
-// 
-//Each integer will be greater than 0 and less than 180.
-// 
-//Output Specification Exactly one of Equilateral, Isosceles, Scalene or Error will be printed on one line.
+// program reads in three angles.
+
 
 public class TriangleTypes {
 	public static void main(String[] args) {
